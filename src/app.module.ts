0ea8df9852308562903845c32cab45348b3f0fc2
tlwsn <dima.dima@cashbulls.io>
2023-07-08 18:27:52 +0300

    fix cron

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL),
     UsersModule,
